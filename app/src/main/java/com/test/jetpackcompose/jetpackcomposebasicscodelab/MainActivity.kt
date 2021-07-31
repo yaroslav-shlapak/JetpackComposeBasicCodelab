93bf03265347d00e1ea89c9aaacfcf04303eba12
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-07-31 23:16:37 +0300

    complete part 4

--- app/src/main/java/com/test/jetpackcompose/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/test/jetpackcompose/jetpackcomposebasicscodelab/MainActivity.kt
@@ -3,39 +3,59 @@ package com.test.jetpackcompose.jetpackcomposebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.test.jetpackcompose.jetpackcomposebasicscodelab.ui.theme.JetpackComposeBasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Yellow) {
-        Text(text = "Hello $name!")
+private fun MyApp(content: @Composable () -> Unit) {
+    JetpackComposeBasicsCodelabTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = Color.Yellow) {
+            content()
+        }
     }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "there", "how's it going?")) {
+    Column {
+        for (name in names) {
+            Greeting(name)
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeBasicsCodelabTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
