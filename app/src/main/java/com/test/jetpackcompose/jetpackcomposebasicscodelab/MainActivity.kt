78efb2c7967cd634f40e23bbd4a7d3842e77904a
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-08-09 20:04:20 +0300

    complete part 5

--- app/src/main/java/com/test/jetpackcompose/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/test/jetpackcompose/jetpackcomposebasicscodelab/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,11 +41,22 @@ private fun MyApp(content: @Composable () -> Unit) {
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "there", "how's it going?")) {
+    val counterState = remember {
+        mutableStateOf(0)
+    }
+
     Column {
         for (name in names) {
             Greeting(name)
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount ->
+                counterState.value = newCount
+            }
+        )
     }
 }
 
@@ -50,6 +64,14 @@ fun MyScreenContent(names: List<String> = listOf("Android", "there", "how's it g
 @Composable
 fun Greeting(name: String) {
         Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit ) {
+
+    Button(onClick = { updateCount(count + 1) }) {
+        Text(text = "I've been clicked $count times")
+    }
 }
 
 @Preview(showBackground = true)
