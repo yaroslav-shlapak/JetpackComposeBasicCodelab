760b0b7593ee1b212d8a9ca5bcd33fde13e88bf8
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-08-11 17:35:30 +0300

    complete part 6

--- app/src/main/java/com/test/jetpackcompose/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/test/jetpackcompose/jetpackcomposebasicscodelab/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -40,23 +44,29 @@ private fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there", "how's it going?")) {
+fun MyScreenContent(names: List<String> = List(1000) { "Hello Android #$it" } ) {
     val counterState = remember {
         mutableStateOf(0)
     }
 
-    Column {
-        for (name in names) {
-            Greeting(name)
-            Divider(color = Color.Black)
-        }
-        Divider(color = Color.Transparent, thickness = 32.dp)
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(names, Modifier.weight(1f))
         Counter(
             count = counterState.value,
             updateCount = { newCount ->
                 counterState.value = newCount
             }
         )
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
+            Greeting(name)
+            Divider(color = Color.Black)
+        }
     }
 }
 
@@ -68,8 +78,12 @@ fun Greeting(name: String) {
 
 @Composable
 fun Counter(count: Int, updateCount: (Int) -> Unit ) {
-
-    Button(onClick = { updateCount(count + 1) }) {
+    Button(
+        onClick = { updateCount(count + 1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count > 5) Color.Green else Color.White
+        )
+    ) {
         Text(text = "I've been clicked $count times")
     }
 }
