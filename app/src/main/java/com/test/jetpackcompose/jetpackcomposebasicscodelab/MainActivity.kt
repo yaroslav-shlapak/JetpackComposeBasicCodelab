d1149b80f34f0c7add88ae3f9560b541dc0851a9
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-08-11 18:52:15 +0300

    complete part 7

--- app/src/main/java/com/test/jetpackcompose/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/test/jetpackcompose/jetpackcomposebasicscodelab/MainActivity.kt
@@ -3,6 +3,9 @@ package com.test.jetpackcompose.jetpackcomposebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
@@ -14,8 +17,10 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -73,7 +78,16 @@ fun NameList(names: List<String>, modifier: Modifier = Modifier) {
 
 @Composable
 fun Greeting(name: String) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    var isSelected by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState( if (isSelected) Color.Red else Color.Transparent)
+
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier
+                .padding(24.dp)
+                .background(color = backgroundColor)
+                .clickable(onClick = { isSelected = !isSelected })
+        )
 }
 
 @Composable
